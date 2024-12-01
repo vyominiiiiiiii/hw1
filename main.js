9966719fee4f128bca80204c737d74f745583317
vyominiiiiiiii <102034432+vyominiiiiiiii@users.noreply.github.com>
2024-12-01 20:07:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var name="Google";
+console.log(name);
+console.log(typeof name); //string
+
+var num=500;
+console.log(num);
+console.log(typeof num);    //number
+
+var boolean=true;
+console.log(boolean);
+console.log(typeof boolean);    //boolean
+
+
+var empty;
+console.log(empty);
+console.log(typeof empty);  //undefined
+
+var num2=4;
+
+console.log(num+num2);
+console.log(num-num2);
+console.log(num/num2);
+console.log(num*num2);
+console.log(num%num2);
+
